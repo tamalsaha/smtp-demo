35df670c0c1c17881fa4c68381156260e980f87c
Tamal Saha <tamal@appscode.com>
2022-05-02 17:06:59 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tamalsaha/smtp-demo
 
 go 1.18
+
+require github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import "github.com/jordan-wright/email"
+
+func main() {
+	e := email.NewEmail()
+	e.From = "Jordan Wright <test@gmail.com>"
+	e.To = []string{"test@example.com"}
+	e.Bcc = []string{"test_bcc@example.com"}
+	e.Cc = []string{"test_cc@example.com"}
+	e.Subject = "Awesome Subject"
+	e.Text = []byte("Text Body is, of course, supported!")
+	e.HTML = []byte("<h1>Fancy HTML is supported, too!</h1>")
+	e.Send("smtp.gmail.com:587", smtp.PlainAuth("", "test@gmail.com", "password123", "smtp.gmail.com"))
+}
